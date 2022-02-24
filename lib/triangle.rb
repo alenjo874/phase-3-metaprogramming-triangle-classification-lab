552dbcdeeb1572474489830b4d34ba678d3cb096
Alen Jo <alenjo874@gmail.com>
2022-02-24 15:20:11 -0500

    commit tree 250c76d1dbf367623b1c64b502dc8d793e15ffcb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
-  # write code here
+
+  def initialization(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    if self.side_1 == self.side_2 && self.side_2 == self.side_3
+      return  ":equilateral"
+    end
+  end
 end
